d24457f7cb458f4b156b51f2e5220ba698ca433d
nornjuidat <njuidatnor750@gmail.com>
2026-06-11 22:29:06 +0300

    crate the router  fix

--- server/Router/main_api_R.js
+++ server/Router/main_api_R.js
@@ -1,6 +1,3 @@
 const express = require('express');
 const router = express.Router();
 module.exports = router;
-
-const Attendance_R = require('./Attendance_R');
-router.use('/AE',[],Attendance_R);
