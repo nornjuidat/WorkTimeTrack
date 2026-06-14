b7d26e2e4b5e6262bce07bbd128819c19e895e67
nornjuidat <njuidatnor750@gmail.com>
2026-06-15 00:36:56 +0300

    add application header

---
+++ clint/src/Layout/Header.jsx
@@ -0,0 +1,46 @@
+import React,{useEffect} from 'react';
+import { AppBar, Toolbar, Typography, IconButton, useMediaQuery, useTheme, Box
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { HeaderHeight } from "../theme_params";
+
+function Header({ onMenuClick }) {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <AppBar
+            position="static"
+            sx={{
+                height: `${HeaderHeight}px`,
+                '& .MuiToolbar-root': {
+                    height: `${HeaderHeight}px`,
+                    minHeight: `${HeaderHeight}px`
+                }
+            }}
+        >
+            <Toolbar>
+                {isMobile && (
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={onMenuClick}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                )}
+                <Typography variant="h6" component="h1" sx={{pr:3}}>
+                Work Time Tracker
+                </Typography>
+        
+                {/* Spacer to push content to the edges */}
+                <Box sx={{ flexGrow: 1 }} />
+
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
